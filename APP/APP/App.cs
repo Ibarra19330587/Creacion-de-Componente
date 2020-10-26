7c894de6c52476ef414c3d272d7c7367ebb75d97
Ibarra19330587 <72420795+Ibarra19330587@users.noreply.github.com>
2020-10-26 15:32:06 -0700

    Add files via upload

---
+++ APP/APP/App.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace APP
+{
+    public partial class App : UserControl
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bienvenidos a Ibaes Decoración" +
+                "Aqui podrás encontrar Variedad de persianas " +
+                "a un buen precio y excelente calidad");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Plaza Colosio" +
+                "Avenida los bukis #1235");
+        }
+    }
+}
